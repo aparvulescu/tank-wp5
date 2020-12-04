124b9f6800618adfd82c2a278e2fd45d79e93cde
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2020-12-04 12:34:53 +0100

    Final of session 2
    Co-authored-by: jasper lapidaire <jasper.lapidaire@hotmail.nl>
    Co-authored-by: Kyoung Eun LEE <kyoungeunlee@tudelft.nl>
    Co-authored-by: Yoni Verhulst <yoni.verhulst1@outlook.com>
    Co-authored-by: Tristan Hirs <thirs@tudelft.nl>
    Co-authored-by: Gerwin van den Heuvel <gerwinvandenheuvel@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from launch_loads import bucklingCheck
 from materialselection import MatSelec
 from mass_calculators import massTankAndProp
+from mass_attachments import massAttachments
 import numpy as np
 import math
 
@@ -12,38 +13,64 @@ density = [4430, 4480, 2840]
 V = [0.12/2, 0.15/2]
 fo = 2
 ok = 0
+n = 4  # number of attachemnts per tank
 
 while fo != 0 and fo != 1:
-    fo = int(input("Do you want to calculate my ass for fuel (0) or oxizidizer (1)?: "))
+    fo = int(input("Do you want to calculate my posterior for fuel (0) or oxizidizer (1)?: "))
     if fo != 0 and fo != 1:
         print("Your input is wrong! Only accepted values are 0 (fuel) or 1 (oxidizer). Try again.")
     
 maxR = (3 * V[fo] / 4 / math.pi)**(1/3)
 
-for R in np.arange(0.001, maxR, 1e-3):
+for R in np.arange(0.01, maxR, 1e-3):
     L = 1 / (math.pi * R * R) * (V[fo] - 4/3*math.pi*R*R*R)
-
+    if (L > 1.0 or L < 0):
+        continue
     at1, at2 = MatSelec(R, yieldstrength)
 
     MLst = []
+    t1lst = []
+    t2lst = []
     for i in range(len(materials)):
-        totMass = massTankAndProp(i, L, R, at1[i], at2[i], fo)
+        t1 = at1[i]
+        t2 = at2[i]
+        totMass = massTankAndProp(i, L, R, t1, t2, fo)
+        diff_col, diff_shell = bucklingCheck(i, L, R, t1, totMass)
+        while (diff_col < 0 or diff_shell < 0) and t1 < 0.5e-2:
+            t1 += 1e-5
+            totMass = massTankAndProp(i, L, R, t1, t2, fo)
+            diff_col, diff_shell = bucklingCheck(i, L, R, t1, totMass)
         MLst.append(totMass)
-    
+        t1lst.append(t1)
+        t2lst.append(t2)
+
     lowestMass = min(MLst)
     im = MLst.index(min(MLst))
-    t1 = at1[im]
-    t2 = at2[im]
+    t1 = t1lst[im]
+    t2 = t2lst[im]
     material = materials[im]
-    
-    diff_col, diff_shell = bucklingCheck(im, L, R, t1, lowestMass)
 
     if diff_col > 0 and diff_shell > 0 and L > 0:
-        print(f"Option passes with material = {material}, L = {L:.4f}, R = {R:.4f}, t1 = {t1:.4f}, t2 = {t2:.4f}, mass = {lowestMass:.4f}")
+        #print(f"Option passes with material = {material}, L = {L:.5f}, R = {R:.5f}, t1 = {t1:.5f}, t2 = {t2:.5f}, mass = {lowestMass:.5f}")
         ok = 1
+        
+        oldMass = lowestMass
+        matt = massAttachments(oldMass, n)
+        newMass = oldMass + matt
+        while newMass - oldMass <= 0.001:
+            oldMass = newMass
+            matt = massAttachments(oldMass,n)
+            newMass = oldMass + matt
+        # to add buckling check
+        # including the attachents
+        diff_col, diff_shell = bucklingCheck(i, L, R, t1, newMass) 
+        if diff_col > 0 and diff_shell > 0:
+            print(f"Option passes with material = {material}, L = {L:.5f}, R = {R:.5f}, t1 = {t1:.5f}, t2 = {t2:.5f}, mass = {lowestMass:.5f}")
+            
     else:
-        print(f"diff_col = {diff_col}, diff_shell = {diff_shell}")
+        #print(f"diff_col = {diff_col}, diff_shell = {diff_shell}")
         continue
 
 if ok == 0:
     print("No suitable value found.")
+
--- mass_attachments.py
+++ mass_attachments.py
@@ -1,17 +1,19 @@
 import math
 
-Fx = 1427.40
-Fy = 899.8 #FY + F1
-Fz = 665.70
-g = 9.81
-F_tot = math.sqrt(Fx**2 + Fy**2 + Fz**2)
-m_wp4 = 0.0233
+
 
 
 def massAttachments(m, n): # m = mass of tank, n = number of attachments, m_wp4 = mass of lug attachment
+    Fx = 1427.40
+    Fy = 899.8 #FY + F1
+    Fz = 665.70
+    g = 9.81
+    F_tot = math.sqrt(Fx**2 + Fy**2 + Fz**2)
+    m_wp4 = 0.0233
+    
     P = 4.3 * g * m
     P_n = P / n
-    ratio = F_tot / P_n
+    ratio = P_n / F_tot
     m_att = ratio * m_wp4
     m_atttot = m_att * n
     
