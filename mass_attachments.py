39b6d32cc93ea5087ff24839354ff7980d1ee5c6
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2020-12-16 21:36:00 +0200

    End of session 6
    Co-authored-by: Gerwin van den Heuvel <gerwinvandenheuvel@gmail.com>
    Co-authored-by: Kyoung Eun LEE <kyoungeunlee@tudelft.nl>

--- mass_attachments.py
+++ mass_attachments.py
@@ -1,18 +1,15 @@
 import math
 
-
-
-
 def massAttachments(m, n): # m = mass of tank, n = number of attachments, m_wp4 = mass of lug attachment
     Fx = 1427.40
     Fy = 899.8 #FY + F1
     Fz = 665.70
-    m_sc = 1477.5
+    m_sc = 1477.5 - 240.3
     g = 9.81
     F_tot = math.sqrt(Fx**2 + Fy**2 + Fz**2)
     m_wp4 = 0.0233
     
-    P = 4.3 * g * (m + m_sc)
+    P = 4.3 * g * (m + m_sc/4)
     P_n = P / n
     ratio = P_n / F_tot
     m_att = ratio * m_wp4
